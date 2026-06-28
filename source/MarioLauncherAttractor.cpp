5d1d0616c786de03305068a1c182f86b6cecdc66
gaimerI17 <gaimeri255@gmail.com>
2026-06-29 01:51:47 +0300

    Update MarioLauncherAttractor.cpp

--- source/MarioLauncherAttractor.cpp
+++ source/MarioLauncherAttractor.cpp
@@ -2,7 +2,7 @@
 
 namespace NrvMarioLauncherAttractor {
     FULL_NERVE(MarioLauncherAttractorNrvWait, MarioLauncherAttractor, Wait);
-};  // namespace NrvMarioLauncherAttractor
+};
 
 MarioLauncherAttractor::MarioLauncherAttractor(const char* pName) : LiveActor(pName) {
     mGravityCreator = nullptr;
@@ -12,26 +12,19 @@ void MarioLauncherAttractor::init(const JMapInfoIter& rIter) {
     MR::initDefaultPos(this, rIter);
     initModelManagerWithAnm("MarioLauncherAttractor", nullptr, nullptr, false);
 
-    // Create and initialize gravity
     mGravityCreator = new PointGravityCreator();
     mGravityCreator->createFromJMap(rIter);
     PlanetGravity* pGravity = mGravityCreator->getGravity();
     pGravity->mGravityType = GRAVITY_TYPE_MARIO_LAUNCHER;
 
-    // Set clipping & scale
     f32 clipping = pGravity->mRange / 1000.0f;
     MR::setClippingTypeSphere(this, pGravity->mRange);
     MR::setClippingFar200m(this);
 
-    TVec3f _8;  // unused
-    _8.x = clipping;
-    _8.y = clipping;
-    _8.z = clipping;
     mScale.x = clipping;
     mScale.y = clipping;
     mScale.z = clipping;
 
-    // Connect to executor, init nerve and start animation
     MR::connectToSceneMapObj(this);
     initNerve(&NrvMarioLauncherAttractor::MarioLauncherAttractorNrvWait::sInstance, 0);
     MR::startBtk(this, "MarioLauncherAttractor");
